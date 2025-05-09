09bccd587f61839c94bf55dbc00a36503980c06f
sese <jangsejin11111@gmail.com>
2025-05-09 17:22:31 +0900

    오프닝 씬 테스트 추가
    [씬 위치]
    sejin > Scenes > 0509_Opening_TEST
    
    - 빌드 프로파일에 테스트용 씬 2개 '0509_~' 추가함

---
+++ Assets/sejin/Scripts/IntroController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour
+{
+    [SerializeField] private string mainSceneName = "0509_SceneChanger_TEST";
+
+    // UI Button OnClick에 연결할 메서드
+    public void OnStartButton()
+    {
+        // 씬 전환. 동기 로드면 짧게 끊김.
+        SceneManager.LoadScene(mainSceneName);
+        // 혹은 비동기:
+        // StartCoroutine(LoadAsync());
+
+        // IEnumerator LoadAsync()
+        // {
+        //     AsyncOperation op = SceneManager.LoadSceneAsync(mainSceneName);
+        //     while (!op.isDone) yield return null;
+        // }
+    }
+}
+
+
